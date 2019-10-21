4e67a416cf85ccf7817163ca7ceab1a66b62f094
Marcello Amaral Florido <marcello.florido@toroinvestimentos.com.br>
2019-10-21 13:47:26 -0300

    nova api

---
+++ WebApi2/Controllers/CalculaJurosController.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculaJurosController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return new List<WeatherForecast>();
+        }
+    }
+}
