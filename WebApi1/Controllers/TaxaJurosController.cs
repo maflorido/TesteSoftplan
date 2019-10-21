e9bcd7bf7e0db2e4fe61a22ee09f1a8dff160888
Marcello Amaral Florido <marcello.florido@toroinvestimentos.com.br>
2019-10-21 09:04:40 -0300

    api taxa de juros

---
+++ WebApi1/Controllers/TaxaJurosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxaJurosController : ControllerBase
+    {
+        private static decimal _taxaJuros = 0.01M;
+        private readonly ILogger<TaxaJurosController> _logger;
+
+        public TaxaJurosController(ILogger<TaxaJurosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public decimal Get()
+        {
+            return _taxaJuros;
+        }
+    }
+}
